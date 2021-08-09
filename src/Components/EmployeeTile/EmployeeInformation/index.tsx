5f69b9e0a21776b529ea5da25744ad9033e93666
Gregory Krynski <gregory.krynski@gmail.com>
2021-08-10 01:07:40 +0900

    add gitignore and add extra feild editablity

--- src/Components/EmployeeTile/EmployeeInformation/index.tsx
+++ src/Components/EmployeeTile/EmployeeInformation/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 const EmployeeInformation = ({ display }: { display: boolean }) => {
-  const [edit, setEdit] = useState({ postalCode: false });
+  const [edit, setEdit] = useState({ postalCode: false, address: false });
 
-  const [state, setState] = useState({ postalCode: "134 444" });
+  const [state, setState] = useState({
+    postalCode: "134 444",
+    address: "1234 rockafeller st",
+  });
 
   return (
     <div
@@ -22,20 +25,44 @@ const EmployeeInformation = ({ display }: { display: boolean }) => {
       <div className="data-row">Country:</div>
       <div className="data-row">Province/State:</div>
       <div className="data-row">City:</div>
-      <div className="data-row">Address:</div>
+      <div className="data-row">
+        <div>Address:</div>
+        <div>
+          {edit.address ? (
+            <input
+              type="text"
+              onChange={(event) =>
+                setState({ ...state, address: event.target.value })
+              }
+              onKeyPress={(event) => {
+                //need simplification and refactoring will create a function later
+                if (event.key === "Enter") {
+                  setEdit({ ...edit, address: !edit.address });
+                }
+              }}
+              value={state.address}
+            />
+          ) : (
+            <p onClick={() => setEdit({ ...edit, address: !edit.address })}>
+              {state.address}
+            </p>
+          )}
+        </div>
+      </div>
       <div className="data-row">City:</div>
       <div className="data-row">
-        <div>Postal Code:</div> 
-        <div>  
+        <div>Postal Code:</div>
+        <div>
           {edit.postalCode ? (
             <input
               type="text"
               onChange={(event) =>
-                setState({ ...edit, postalCode: event.target.value })
+                setState({ ...state, postalCode: event.target.value })
               }
-              onKeyPress={event => { //need simplification
+              onKeyPress={(event) => {
+                //need simplification
                 if (event.key === "Enter") {
-                  setEdit({ ...edit, postalCode: !edit.postalCode})
+                  setEdit({ ...edit, postalCode: !edit.postalCode });
                 }
               }}
               value={state.postalCode}
